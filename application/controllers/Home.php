aac3fa3470810f1c8cd1e51a5cef53042d0e25b9
firdausillah <firdausillah123@gmail.com>
2021-10-15 11:18:33 +0700

    Initial commit

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,19 +3,34 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-	function __construct()
-	{
+	function __construct(){
 		parent::__construct();
 		$this->load->model('GelombangModel', 'mGelombang');
+		$this->load->model('JurusanModel', 'mJurusan');
 	}
 
-
-	public function index()
-	{
+	public function index(){
 		$data = [
-			'gelombangs' => $this->mGelombang->get()->result()
+			'gelombangs' => $this->mGelombang->get()->result(),
+			'jurusans' => $this->mJurusan->get()->result()
 		];
-
+		// $this->session->set_flashdata('success', $data);
 		$this->load->view('welcome_message', $data);
+	}
+
+	public function save(){
+		// print_r($_POST['nama']);
+		$data = $_POST;
+		// exit();
+		// $data = null;
+
+		// if ($this->mGelombang->add($data)) {
+		if ($data) {
+			$this->session->set_flashdata('success', $data);
+		} else {
+			$this->session->set_flashdata('error', 'Oops! Terjadi suatu kesalahan');
+		}
+
+		redirect(base_url('home'));
 	}
 }
