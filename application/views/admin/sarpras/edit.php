2e8c11cb039bff989d0d29e1e673c0d0763704db
Your Name <you@example.com>
2023-03-27 23:14:27 +0700

    update sarpras

--- application/views/admin/sarpras/edit.php
+++ application/views/admin/sarpras/edit.php
@@ -9,16 +9,29 @@
     <div class="row">
         <div class="col-12 col-lg-12 col-xxl-9 d-flex">
             <div class="card flex-fill">
-                <?= form_open_multipart(base_url('admin/marketing/update/' . $pembawa->id)) ?>
+                <?= form_open_multipart(base_url('admin/sarpras/update/' . $sarpras->id)) ?>
                 <div class="card-body">
                     <div class="mb-3">
-                        <label class="form-label">Marketing</label>
-                        <input type="text" class="form-control" name="nama_lengkap" value="<?= $pembawa->nama_lengkap ?>" required>
+                        <label class="form-label">kode</label>
+                        <input type="input" class="form-control" name="kode" value="<?= $sarpras->kode ?>" required>
                     </div>
+                    <div class="mb-3">
+                        <label class="form-label">sarpras</label>
+                        <input type="input" class="form-control" name="nama" value="<?= $sarpras->nama ?>" required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">foto</label>
+                        <input type="file" class="form-control" name="foto" value="<?= $sarpras->foto ?>" required>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">keterangan</label>
+                        <input type="text" class="form-control" name="keterangan" value="<?= $sarpras->keterangan ?>" required>
+                    </div>
+
 
                 </div>
                 <div class="card-footer text-right">
-                    <a href="<?= base_url() ?>admin/marketing" class="btn btn-secondary btn-sm">Kembali</a>
+                    <a href="<?= base_url() ?>admin/sarpras" class="btn btn-secondary btn-sm">Kembali</a>
                     <button type="submit" class="btn btn-success btn-sm">Simpan</button>
                 </div>
                 </form>
