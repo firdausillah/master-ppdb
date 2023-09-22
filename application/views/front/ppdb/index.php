edc21361a25f5779ef052fa34e1af2188bcb5527
firdausillah <firdausillah123@gmail.com>
2023-09-22 07:10:12 +0700

    update ppdb

--- application/views/front/ppdb/index.php
+++ application/views/front/ppdb/index.php
@@ -178,7 +178,7 @@
                   <h5 class="modal-title" id="exampleModalLabel">Form Pendaftaran Siswa Baru</h5>
                   <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
-              <?= form_open_multipart('home/save'); ?>
+              <?= form_open_multipart('ppdb/save'); ?>
               <div class="modal-body">
                   <div class="mb-3">
                       <label class="form-label">Nama Lengkap</label>
